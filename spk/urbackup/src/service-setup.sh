d1173c36fc152963e32d37a60af9bcab865d18fd
josef109 <josefglaze@gmail.com>
2022-04-26 21:42:05 +0200

    remove unused

--- spk/urbackup/src/service-setup.sh
+++ spk/urbackup/src/service-setup.sh
@@ -11,27 +11,15 @@ SERVICE_COMMAND="${SYNOPKG_PKGDEST}/bin/urbackupsrv run -d -v error -u sc-urback
 
 SVC_BACKGROUND=y
 SVC_WRITE_PID=y
-#GROUP="root"
 
 service_postinst ()
 {
    	mkdir -p ${SYNOPKG_PKGVAR}/urbackup
-    #ln -s ${SYNOPKG_PKGDEST} /etc/urbackup
-    #echo "tank/images" > ${SYNOPKG_PKGDEST}/var/urbackup/dataset  
     echo "${SYNOPKG_PKGHOME}" > ${SYNOPKG_PKGVAR}/urbackup/backupfolder 
-    #synogroup --add "administrators" "sc-urbackup" > /dev/null
-    #addgroup "sc-urbackup" "administrators"
     sed -i 's/package/root/g' /var/packages/urbackup/conf/privilege
 }
 
 service_prestart ()
 {
     CONFIG_DIR="${SYNOPKG_PKGVAR}"
-
-    #synogroup --add "administrators" ${USER} > /dev/null
-    #addgroup ${USER} "administrators"
-
-    # Required: start-stop-daemon do not set environment variables
-    #HOME=${SYNOPKG_PKGDEST}/var
-    #export HOME
 }
